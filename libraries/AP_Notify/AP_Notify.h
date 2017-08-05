snehashahi/rmackay9-ardupilot

AP_Notify: add gps glitch notification

libraries/AP_Notify/AP_Notify.h
@@ -76,6 +76,7 @@ public:
         uint32_t leak_detected      : 1;    // 1 if leak detected
         float    battery_voltage       ;    // battery voltage
         uint32_t gps_fusion         : 1;    // 0 = GPS fix rejected by EKF, not usable for flight. 1 = GPS in use by EKF, usable for flight
+        uint32_t gps_glitch         : 1;    // 1 if gps is glitching
 
         // additional flags
         uint32_t external_leds      : 1;    // 1 if external LEDs are enabled (normally only used for copter)

fdzyffff/ardupilot

libraries/AP_Notify/AP_Notify.h
@@ -71,6 +71,7 @@ public:
         uint32_t leak_detected      : 1;    // 1 if leak detected
         float    battery_voltage       ;    // battery voltage
         uint32_t gps_fusion         : 1;    // 0 = GPS fix rejected by EKF, not usable for flight. 1 = GPS in use by EKF, usable for flight
+        uint32_t gps_glitching      : 1;    // 1 if gps is glitching
 
         // additional flags
         uint32_t external_leds      : 1;    // 1 if external LEDs are enabled (normally only used for copter)