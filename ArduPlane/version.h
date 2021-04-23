7347da3fb98752182a65cdf68eefbe88478af6d7
fdzyffff <1069379433@qq.com>
2021-04-23 19:08:46 +0800

    v35v1

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.0.3-35"
+#define THISFIRMWARE "ArduPlane V4.0.3-35v1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,3,FIRMWARE_VERSION_TYPE_OFFICIAL
