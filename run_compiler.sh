ce8689d0a130d12732169ed0c81772692e64a2e9
fdzyffff <1069379433@qq.com>
2024-03-23 21:28:36 +0800

    20230323

--- run_compiler.sh
+++ run_compiler.sh
@@ -1,9 +1,19 @@
-./waf configure --board 0x7nora
+
+mkdir firmare
+rm -fr firmare/*
+echo "~~~~~~~~~~~~~~~ start ~~~~~~~~~~~~~~~"
+./waf configure --board 0X7nora
 ./waf plane
-echo "next"
-./waf configure --board 0x7pilot
+mkdir firmare/0X7nora
+cp build/0X7nora/bin/arduplane.apj firmare/0X7nora/
+echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
+./waf configure --board 0X7pilot
 ./waf plane
-echo "next"
-./waf configure --board cuav-x7
+mkdir firmare/0X7pilot
+cp build/0X7pilot/bin/arduplane.apj firmare/0X7pilot/
+echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
+./waf configure --board CUAV-X7
 ./waf plane
-echo "Done"
+mkdir firmare/CUAV-X7
+cp build/CUAV-X7/bin/arduplane.apj firmare/CUAV-X7/
+echo "~~~~~~~~~~~~~~~ done ~~~~~~~~~~~~~~~"
