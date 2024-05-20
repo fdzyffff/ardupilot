b101696eb8d38b5ead09d6fc81f272d25bb23fac
fdzyffff <1069379433@qq.com>
2024-05-20 16:11:37 +0800

    20240520

--- ArduCopter/mode_stabilize.cpp
+++ ArduCopter/mode_stabilize.cpp
@@ -11,6 +11,7 @@ void ModeStabilize::run()
     // apply simple mode transform to pilot inputs
     update_simple_mode();
 
+    // update_attitude_mode();
     // convert pilot input to lean angles
     float target_roll, target_pitch;
     get_pilot_desired_lean_angles(target_roll, target_pitch, copter.aparm.angle_max, copter.aparm.angle_max);
@@ -62,9 +63,16 @@ void ModeStabilize::run()
         break;
     }
 
+    float target_forward = -target_pitch/4500.f;
+    float target_lateral = target_roll/4500.f;
+    target_roll = 0.0f;
+    target_pitch = 0.0f;
     // call attitude controller
     attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
 
     // output pilot's throttle
     attitude_control->set_throttle_out(pilot_desired_throttle, true, g.throttle_filt);
+
+    motors->set_forward(target_forward);
+    motors->set_lateral(target_lateral);
 }
