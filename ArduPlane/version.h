cd3dba06b6aae99cb6ffdd8848d8b1005c088758
fdzyffff <1069379433@qq.com>
2024-09-19 19:46:25 +0800

    20240919

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.0.3-v1.0-L"
+#define THISFIRMWARE "ArduPlane V4.0.3-v1.1-LD"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,3,FIRMWARE_VERSION_TYPE_OFFICIAL
