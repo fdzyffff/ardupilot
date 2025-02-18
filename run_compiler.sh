3c5647c2fe0e5da37fdc8ab305cbc2afea8bde9f
fdzyffff <1069379433@qq.com>
2025-02-18 20:00:03 +0800

    20250218

--- run_compiler.sh
+++ run_compiler.sh
@@ -5,21 +5,21 @@ rm -fr firmare/*
 echo "~~~~~~~~~~~~~~~ start ~~~~~~~~~~~~~~~"
 ./waf configure --board APzH7
 ./waf copter
-mkdir firmare/APzH7
-cp build/APzH7/bin/arducopter.apj firmare/APzH7/$current_datetime\_arducopter.apj
+# mkdir firmare/APzH7
+cp build/APzH7/bin/arducopter.apj firmare/$current_datetime\_APzH7_arducopter.apj
 echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
 ./waf configure --board APzH7
 ./waf heli
-mkdir firmare/APzH7
-cp build/APzH7/bin/arducopter-heli.apj firmare/APzH7/$current_datetime\arducopter-heli.apj
+# mkdir firmare/APzH7
+cp build/APzH7/bin/arducopter-heli.apj firmare/$current_datetime\_APzH7_arducopter-heli.apj
 echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
 ./waf configure --board APzF4
 ./waf copter
-mkdir firmare/APzF4
-cp build/APzF4/bin/arducopter.apj firmare/APzF4/$current_datetime\arducopter.apj
+# mkdir firmare/APzF4
+cp build/APzF4/bin/arducopter.apj firmare/$current_datetime\_APzF4_arducopter.apj
 echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
 ./waf configure --board APzF4
 ./waf heli
-mkdir firmare/APzF4
-cp build/APzF4/bin/arducopter-heli.apj firmare/APzF4/$current_datetime\arducopter-heli.apj
+# mkdir firmare/APzF4
+cp build/APzF4/bin/arducopter-heli.apj firmare/$current_datetime\_APzF4_arducopter-heli.apj
 echo "~~~~~~~~~~~~~~~ done ~~~~~~~~~~~~~~~"
