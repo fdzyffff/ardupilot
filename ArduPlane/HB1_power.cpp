703f2cf5114668438d759e4d9a6402fcdd6f27cf
fdzyffff <1069379433@qq.com>
2025-04-09 19:14:00 +0800

    20250409

--- ArduPlane/HB1_power.cpp
+++ ArduPlane/HB1_power.cpp
@@ -363,39 +363,42 @@ void Plane::HB1_msg_apm2power_set_rocket_on() {
 }
 
 void Plane::HB1_Power_throttle_update() {
-    bool dont_send_throttle = false;
-    switch (HB1_Power.state) {
-        case HB1_PowerAction_GROUND_EngineSTART_PRE:
-            dont_send_throttle = true;
-            break;
-        case HB1_PowerAction_None:
-        case HB1_PowerAction_RocketON:
-        case HB1_PowerAction_EnginePullUP:
-        case HB1_PowerAction_EngineON:
-        case HB1_PowerAction_EngineOFF:
-        case HB1_PowerAction_ParachuteON:
-        case HB1_PowerAction_GROUND_RocketON:
-        case HB1_PowerAction_GROUND_EngineSTART:
-        case HB1_PowerAction_GROUND_EngineON:
-        case HB1_PowerAction_GROUND_EngineOFF:
-        case HB1_PowerAction_GROUND_EngineFULL:
-        case HB1_PowerAction_GROUND_EngineMID:
-        default:
-            break;
-    }
-    if (dont_send_throttle) {return;}
-    float throttle_out = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle_HB1);
     HB1_apm2power &tmp_msg = HB1_uart_power.get_msg_apm2power();
+    tmp_msg.set_engine_throttle_control();
     tmp_msg._msg_1.need_send = true;
-    // tmp_msg.set_throttle(throttle_out);
-    uint16_t rpm_half = 17500 + (uint16_t)(throttle_out*500.f*0.5f);
-    if (throttle_out < g2.hb1_engine60_min.get()) {
-        rpm_half = 17500;
-    }
-    tmp_msg.set_rpm_half(rpm_half);
-    tmp_msg.make_sum();
-    tmp_msg._msg_1.print = false;
-    return;
+    // bool dont_send_throttle = false;
+    // switch (HB1_Power.state) {
+    //     case HB1_PowerAction_GROUND_EngineSTART_PRE:
+    //         dont_send_throttle = true;
+    //         break;
+    //     case HB1_PowerAction_None:
+    //     case HB1_PowerAction_RocketON:
+    //     case HB1_PowerAction_EnginePullUP:
+    //     case HB1_PowerAction_EngineON:
+    //     case HB1_PowerAction_EngineOFF:
+    //     case HB1_PowerAction_ParachuteON:
+    //     case HB1_PowerAction_GROUND_RocketON:
+    //     case HB1_PowerAction_GROUND_EngineSTART:
+    //     case HB1_PowerAction_GROUND_EngineON:
+    //     case HB1_PowerAction_GROUND_EngineOFF:
+    //     case HB1_PowerAction_GROUND_EngineFULL:
+    //     case HB1_PowerAction_GROUND_EngineMID:
+    //     default:
+    //         break;
+    // }
+    // if (dont_send_throttle) {return;}
+    // float throttle_out = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle_HB1);
+    // HB1_apm2power &tmp_msg = HB1_uart_power.get_msg_apm2power();
+    // tmp_msg._msg_1.need_send = true;
+    // // tmp_msg.set_throttle(throttle_out);
+    // uint16_t rpm_half = 17500 + (uint16_t)(throttle_out*500.f*0.5f);
+    // if (throttle_out < g2.hb1_engine60_min.get()) {
+    //     rpm_half = 17500;
+    // }
+    // tmp_msg.set_rpm_half(rpm_half);
+    // tmp_msg.make_sum();
+    // tmp_msg._msg_1.print = false;
+    // return;
 }
 
 void Plane::HB1_Power_on_send() {
--- libraries/HB1_UART/HB1_apm2power.cpp
+++ libraries/HB1_UART/HB1_apm2power.cpp
@@ -59,53 +59,53 @@ void HB1_apm2power::set_engine_emergency_stop()
 
 void HB1_apm2power::set_throttle(uint8_t thr_in)
 {
-    _msg_1.content.msg.header.head_1 = HB1_apm2power::PREAMBLE1;
-    _msg_1.content.msg.header.head_2 = HB1_apm2power::PREAMBLE2;
-    _msg_1.content.msg.COMM1 = 0xE3;
-    _msg_1.content.msg.COMM2 = 0xE3;
-    _msg_1.content.msg.rpm_h = 0;
-    _msg_1.content.msg.rpm_l = thr_in;
-    _msg_1.content.msg.rel_alt = 0;
-    _msg_1.content.msg.temp = 0;
-    _msg_1.content.msg.setting_flag = 0;
-    _msg_1.content.msg.airspeed = 0;
-    _msg_1.content.msg.byte_11 = 0;
-    _msg_1.content.msg.byte_22 = 0;
-    _msg_1.content.msg.sum = 0;
-    _msg_1.content.msg.xorsum = 0;
+    // _msg_1.content.msg.header.head_1 = HB1_apm2power::PREAMBLE1;
+    // _msg_1.content.msg.header.head_2 = HB1_apm2power::PREAMBLE2;
+    // _msg_1.content.msg.COMM1 = 0xE3;
+    // _msg_1.content.msg.COMM2 = 0xE3;
+    // _msg_1.content.msg.rpm_h = 0;
+    // _msg_1.content.msg.rpm_l = thr_in;
+    // _msg_1.content.msg.rel_alt = 0;
+    // _msg_1.content.msg.temp = 0;
+    // _msg_1.content.msg.setting_flag = 0;
+    // _msg_1.content.msg.airspeed = 0;
+    // _msg_1.content.msg.byte_11 = 0;
+    // _msg_1.content.msg.byte_22 = 0;
+    // _msg_1.content.msg.sum = 0;
+    // _msg_1.content.msg.xorsum = 0;
 }
 
 void HB1_apm2power::set_rpm_half(uint16_t rpm_in)
 {
-    uint8_t rpm_h = ((rpm_in)>>8);
-    uint8_t rpm_l = ((rpm_in)|(0xFF<<8));
-    _msg_1.content.msg.header.head_1 = HB1_apm2power::PREAMBLE1;
-    _msg_1.content.msg.header.head_2 = HB1_apm2power::PREAMBLE2;
-    _msg_1.content.msg.COMM1 = 0xF3;
-    _msg_1.content.msg.COMM2 = 0xF3;
-    _msg_1.content.msg.rpm_h = rpm_h;
-    _msg_1.content.msg.rpm_l = rpm_l;
-    _msg_1.content.msg.rel_alt = 0;
-    _msg_1.content.msg.temp = 0;
-    _msg_1.content.msg.setting_flag = 0;
-    _msg_1.content.msg.airspeed = 0;
-    _msg_1.content.msg.byte_11 = 0;
-    _msg_1.content.msg.byte_22 = 0;
-    _msg_1.content.msg.sum = 0;
-    _msg_1.content.msg.xorsum = 0;
-    // gcs().send_text(MAV_SEVERITY_INFO, "%d, %d", rpm_h, rpm_l);
+    // uint8_t rpm_h = ((rpm_in)>>8);
+    // uint8_t rpm_l = ((rpm_in)|(0xFF<<8));
+    // _msg_1.content.msg.header.head_1 = HB1_apm2power::PREAMBLE1;
+    // _msg_1.content.msg.header.head_2 = HB1_apm2power::PREAMBLE2;
+    // _msg_1.content.msg.COMM1 = 0xF3;
+    // _msg_1.content.msg.COMM2 = 0xF3;
+    // _msg_1.content.msg.rpm_h = rpm_h;
+    // _msg_1.content.msg.rpm_l = rpm_l;
+    // _msg_1.content.msg.rel_alt = 0;
+    // _msg_1.content.msg.temp = 0;
+    // _msg_1.content.msg.setting_flag = 0;
+    // _msg_1.content.msg.airspeed = 0;
+    // _msg_1.content.msg.byte_11 = 0;
+    // _msg_1.content.msg.byte_22 = 0;
+    // _msg_1.content.msg.sum = 0;
+    // _msg_1.content.msg.xorsum = 0;
+    // // gcs().send_text(MAV_SEVERITY_INFO, "%d, %d", rpm_h, rpm_l);
 }
 
 void HB1_apm2power::make_sum()
 {
-    _msg_1.content.msg.header.head_1 = HB1_apm2power::PREAMBLE1;
-    _msg_1.content.msg.header.head_2 = HB1_apm2power::PREAMBLE2;
-    _msg_1.content.msg.sum = 0;
-    _msg_1.content.msg.xorsum = 0;
-    for (int8_t i = 0; i < _msg_1.length - 2; i++) {
-        _msg_1.content.msg.sum += _msg_1.content.data[i];
-    }
-    for (int8_t i = 0; i < _msg_1.length - 2; i++) {
-        _msg_1.content.msg.xorsum = (_msg_1.content.msg.xorsum ^ _msg_1.content.data[i]);
-    }
+    // _msg_1.content.msg.header.head_1 = HB1_apm2power::PREAMBLE1;
+    // _msg_1.content.msg.header.head_2 = HB1_apm2power::PREAMBLE2;
+    // _msg_1.content.msg.sum = 0;
+    // _msg_1.content.msg.xorsum = 0;
+    // for (int8_t i = 0; i < _msg_1.length - 2; i++) {
+    //     _msg_1.content.msg.sum += _msg_1.content.data[i];
+    // }
+    // for (int8_t i = 0; i < _msg_1.length - 2; i++) {
+    //     _msg_1.content.msg.xorsum = (_msg_1.content.msg.xorsum ^ _msg_1.content.data[i]);
+    // }
 }
