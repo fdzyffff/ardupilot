dbe185319950d2286a39838c8062c6289a2a02bb
fdzyffff <1069379433@qq.com>
2025-07-16 00:48:41 +0800

    20250716

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.5.6-TZ605-0.4.5"
+#define THISFIRMWARE "ArduPlane V4.5.6-TZ605-0.4.6"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,6,FIRMWARE_VERSION_TYPE_OFFICIAL
