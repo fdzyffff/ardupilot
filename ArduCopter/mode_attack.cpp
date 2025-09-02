10c8b2201387a7fbc0ab6608c07cab3095495f21
fdzyffff <1069379433@qq.com>
2025-09-02 21:25:56 +0800

    20250902

--- ArduCopter/mode_attack.cpp
+++ ArduCopter/mode_attack.cpp
@@ -68,8 +68,9 @@ void ModeAttack::run()
         break;
     }
 
+    float target_roll_rate = 3.5f*(target_roll - degrees(copter.ahrs.get_roll())*100.f);
     // call attitude controller
-    attitude_control->input_euler_angle_roll_bf_rate_pitch_yaw(target_roll, target_pitch_rate, target_yaw_rate);
+    attitude_control->input_rate_bf_roll_pitch_yaw(target_roll_rate, target_pitch_rate, target_yaw_rate);
 
     float target_throttle = copter.uattack._attack_throttle;
     target_throttle = constrain_float(target_throttle, 0.05f, 1.0f);
