149f4835068ef6178dec93acb056c92b2fe6c3b7
fdzyffff <1069379433@qq.com>
2025-09-11 10:43:18 +0800

    20250911

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
@@ -62,7 +62,9 @@ public:
         // 8 reserved for SBG
         // 9 reserved for EulerNav
         // 10 reserved for Aeron
+#if AP_EXTERNAL_AHRS_ESIM_ENABLED
         ESIM = 90,
+#endif
     };
 
     static AP_ExternalAHRS *get_singleton(void) {
