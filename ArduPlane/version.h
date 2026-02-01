525ec27dddd74ccaedb5a0a66cf0399980232f96
fdzyffff <1069379433@qq.com>
2026-02-02 07:28:05 +0800

    20260202

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.6.3-LS-v0.1.1"
+#define THISFIRMWARE "ArduPlane V4.6.3-LS-v0.0.7"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,3,FIRMWARE_VERSION_TYPE_OFFICIAL
