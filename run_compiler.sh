bd44bfed46bbc6bd9205d7eab08f724a2d01a68b
fdzyffff <1069379433@qq.com>
2026-03-27 22:00:13 +0800

    20260327

--- run_compiler.sh
+++ run_compiler.sh
@@ -4,23 +4,19 @@ pre_fix=$"ER-"
 mkdir firmware
 rm -fr firmware/*
 echo "~~~~~~~~~~~~~~~ start ~~~~~~~~~~~~~~~"
-./waf configure --board 0LDX7
+./waf configure --board 0N7
 ./waf plane
-cp build/0LDX7/bin/arduplane.apj firmware/$pre_fix$current_datetime\_0LDX7_arduplane.apj
+cp build/0N7/bin/arduplane.apj firmware/$pre_fix$current_datetime\_0N7_arduplane.apj
 echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
-./waf configure --board 0LDX7-sim
+./waf configure --board 0N7-sim
 ./waf plane
-cp build/0LDX7-sim/bin/arduplane.apj firmware/$pre_fix$current_datetime\_0LDX7-sim_arduplane.apj
-echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
-./waf configure --board CUAVv5
-./waf plane
-cp build/CUAVv5/bin/arduplane.apj firmware/$pre_fix$current_datetime\_CUAVv5_arduplane.apj
-echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
-./waf configure --board APzH7
-./waf plane
-cp build/APzH7/bin/arduplane.apj firmware/$pre_fix$current_datetime\_APzH7_arduplane.apj
-echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
-./waf configure --board APzH7-sim
-./waf plane
-cp build/APzH7-sim/bin/arduplane.apj firmware/$pre_fix$current_datetime\_APzH7-sim_arduplane.apj
+cp build/0N7-sim/bin/arduplane.apj firmware/$pre_fix$current_datetime\_0N7-sim_arduplane.apj
+# echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
+# ./waf configure --board APzH7
+# ./waf plane
+# cp build/APzH7/bin/arduplane.apj firmware/$pre_fix$current_datetime\_APzH7_arduplane.apj
+# echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
+# ./waf configure --board APzH7-sim
+# ./waf plane
+# cp build/APzH7-sim/bin/arduplane.apj firmware/$pre_fix$current_datetime\_APzH7-sim_arduplane.apj
 echo "~~~~~~~~~~~~~~~ done ~~~~~~~~~~~~~~~"
