07f09a19bb973968aefd5d6ea93f67cb9b690cb0
fdzyffff <1069379433@qq.com>
2026-04-15 16:34:38 +0800

    20260414

--- run_compiler.sh
+++ run_compiler.sh
@@ -7,4 +7,14 @@ echo "~~~~~~~~~~~~~~~ start ~~~~~~~~~~~~~~~"
 ./waf copter
 # mkdir firmware/0LDX7
 cp build/0LDX7/bin/arducopter.apj firmware/$current_datetime\_0LDX7_arducopter.apj
+echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
+./waf configure --board 0LDX7-sim
+./waf copter
+# mkdir firmware/0x7u1.4
+cp build/0LDX7-sim/bin/arducopter.apj firmware/$current_datetime\_0LDX7-sim_arducopter.apj
+echo "~~~~~~~~~~~~~~~ next ~~~~~~~~~~~~~~~"
+./waf configure --board APzH7
+./waf copter
+# mkdir firmware/0x7u1.4
+cp build/APzH7/bin/arducopter.apj firmware/$current_datetime\_APzH7_arducopter.apj
 echo "~~~~~~~~~~~~~~~ done ~~~~~~~~~~~~~~~"
